d4fb1e38cce1241d6d46f785aff78f4ce4cd11ac
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2022-12-17 12:49:09 +0100

    Add animation for model component

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -6,11 +6,13 @@ const exitBtnRefs = document.querySelectorAll('[data-exit]')
 function closeModal() {
     overlayModalRef.classList.remove('overlay-modal--active')
     modalRef.classList.remove('modal--active')
+    overlayModalRef.classList.add('overlay-modal--fade')
 }
 
 btnRef.addEventListener('click', function () {
     overlayModalRef.classList.add('overlay-modal--active')
     modalRef.classList.add('modal--active')
+    overlayModalRef.classList.remove('overlay-modal--fade')
 })
 
 exitBtnRefs.forEach(function (exitBtn) {
